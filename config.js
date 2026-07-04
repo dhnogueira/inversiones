41f602ca800d3feaef3db9d680e6344339a39bab
dhnogueira <dhnogueira@gmail.com>
2026-07-04 20:44:47 +0000

    Deploying to gh-pages from @ dhnogueira/inversiones@63110da119dd8244c0e7d405242985fbfaf59356 🚀

---
+++ config.js
@@ -0,0 +1,6 @@
+// Configuración de Supabase - Reemplaza con tus credeciales reales
+const CONFIG = {
+    SUPABASE_URL: "https://mvefjzyiwjgzhkfahyxi.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12ZWZqenlpd2pnemhrZmFoeXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxODc5NDYsImV4cCI6MjA5ODc2Mzk0Nn0.baHa-_EmuazFt0hsF0BwhPVubD1Um4hs-kBNVDQva0c"
+};
+export default CONFIG;
